f2ac1b47d5fa5996289c8300b3e149d935493ec6
_Yusari_ <itzdinohd@seznam.cz>
2018-03-18 09:05:32 +0100

    Changed Version

--- basicBot.js
+++ basicBot.js
@@ -242,7 +242,7 @@
     var botCreatorIDs = ["3851534", "4105209"];
 
     var basicBot = {
-        version: "2.9.1",
+        version: "2.9.2",
         status: false,
         name: "Taiga",
         loggedInID: null,
